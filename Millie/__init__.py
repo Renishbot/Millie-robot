678f0736939a2a0beb8485c383821f57e07df68a
Jaltahai <114300623+Jaltahai@users.noreply.github.com>
2022-11-24 19:24:17 +0100

    Update __init__.py

--- Millie/__init__.py
+++ Millie/__init__.py
@@ -114,26 +114,26 @@ else:
     OWNER_USERNAME = Config.OWNER_USERNAME
 
     try:
-        DRAGONS = set(int(x) for x in Config.DRAGONS or [1927155351 5570402782 1733484689 1061059757])
-        DEV_USERS = set(int(x) for x in Config.DEV_USERS or [1927155351 5570402782 1733484689 1061059757])
+        DRAGONS = set(int(x) for x in Config.DRAGONS or ['1927155351 5570402782 1733484689 1061059757'])
+        DEV_USERS = set(int(x) for x in Config.DEV_USERS or ['1927155351 5570402782 1733484689 1061059757'])
     except ValueError:
         raise Exception(
             "Your sudo or dev users list does not contain valid BigInteger.")
 
     try:
-        DEMONS = set(int(x) for x in Config.DEMONS or [1927155351 5570402782 1733484689 1061059757])
+        DEMONS = set(int(x) for x in Config.DEMONS or ['1927155351 5570402782 1733484689 1061059757'])
     except ValueError:
         raise Exception(
             "Your support users list does not contain valid BigInteger.")
 
     try:
-        WOLVES = set(int(x) for x in Config.WOLVES or [1927155351 5570402782 1733484689 1061059757])
+        WOLVES = set(int(x) for x in Config.WOLVES or ['1927155351 5570402782 1733484689 1061059757'])
     except ValueError:
         raise Exception(
             "Your whitelisted users list does not contain valid BigInteger.")
 
     try:
-        TIGERS = set(int(x) for x in Config.TIGERS or [1927155351 5570402782 1733484689 1061059757])
+        TIGERS = set(int(x) for x in Config.TIGERS or ['1927155351 5570402782 1733484689 1061059757'])
     except ValueError:
         raise Exception(
             "Your tiger users list does not contain valid BigInteger.")
