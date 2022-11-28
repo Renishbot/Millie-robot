cfbcf8b1c65ea7e7da050c0611d8493c734c8264
Renishbot <117950358+Renishbot@users.noreply.github.com>
2022-11-29 00:36:53 +0100

    Update route.py

--- Millie/route.py
+++ Millie/route.py
@@ -4,4 +4,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Join Telegram Channal https://t.me/R_Mvzz_Group)
+    return web.json_response("Join Telegram Channal https://t.me/R_Mvzz_Group")
