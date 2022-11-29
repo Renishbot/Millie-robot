357a1dbb31c1065f26735e46aa4c5313f0dc0e6b
Renishbot <117950358+Renishbot@users.noreply.github.com>
2022-11-29 22:05:34 +0100

    Update __init__.py

--- Millie/modules/sql/__init__.py
+++ Millie/modules/sql/__init__.py
@@ -1,9 +1,8 @@
-from Millie import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-
+DB_URI = 'postgres://dksngzpj:VLPEE7HaRBITuF7wUluPyDOSR41AtXO7@peanut.db.elephantsql.com/dksngzpj'
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
