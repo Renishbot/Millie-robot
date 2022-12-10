70f4a3493809887f27abe7bdef08952adac7bea8
Renishbot <117950358+Renishbot@users.noreply.github.com>
2022-12-10 06:31:52 +0100

    Update __init__.py

--- Millie/__init__.py
+++ Millie/__init__.py
@@ -9,8 +9,13 @@ import telegram.ext as tg
 from pyrogram import Client, errors, __version__ as pyrover
 from telethon import TelegramClient
 from aiohttp import web
-from .route import routes
 
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("Join Telegram Channal https://t.me/R_Mvzz_Group")
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
